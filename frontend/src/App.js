6e33f9a08c1de48028264a135e8d8d9657c25fd0
Shakil <shakil.hossain450s@gmail.com>
2022-12-10 14:32:26 +0600

    display product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from "./data";
+import { FaCartPlus } from "react-icons/fa";
 
 
 function App() {
@@ -6,7 +8,32 @@ function App() {
       <header>
         <a href="/">amazona</a>
       </header>
-      <main>list Product</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className='products'>
+          {
+            data.products.map((product, i) =>
+              <div className='product bg-slate-100 p-3 shadow-xl border' key={i}>
+                <a href={`/product/${product.slug}`}>
+                  <img className="mb-2 border" src={product.image} alt={product.name} />
+                </a>
+                <div className="ml-1 mt-3">
+                  <p className="text-base font-normal text-gray-500">{product.brand}</p>
+                  <a href={`/product/${product.slug}`}>
+                    <p className="text-lg font-semibold text-gray-800">{product.name}</p>
+                  </a>
+                  <div className="flex justify-between items-center">
+                    <p className="text-gray-600 font-bold">${product.price}</p>
+                    <button className="btn btn-ghost rounded-full btn-outline text-xl">
+                      <FaCartPlus className="w-4 h-4"></FaCartPlus>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )
+          }
+        </div>
+      </main>
     </div>
   );
 }
